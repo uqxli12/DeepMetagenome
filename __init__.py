ee55b5869452627142ce78fd39426a2547bf0069
uqxli12 <xfli@sjziam.ac.cn>
2023-09-21 15:19:56 +0800

    Delete __init__.py

--- __init__.py
+++
@@ -1,7 +0,0 @@
-#this package includes moduals for mining any family of proteins from metagenomes
-
-__all__ = ['Preprocessing_for_multiple_FASTA','Preprocessing_for_Single_FASTA','Deep_Model']
-
-name = "DeepMetagenome"
-
-print('Welcome to DeepMetagenome! Please contact Xiaofang Li for any comments')
